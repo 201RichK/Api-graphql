b07405cf477a6df74233aa088cc5d8002194107c
Amos Pascal Amos <amosamospascal@yahoo.com>
2019-10-19 14:13:14 +0000

    user roles type struct

--- types/users.go
+++ types/users.go
@@ -10,12 +10,37 @@ type Users struct {
 	Lastname  string `json:"lastname"`
 }
 
-//usertype is use bys the GRAPHQL API to specify which field can be acces
+//user role definition
+type Role struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+//usertype is use bys the GRAPHQL API to specify which field can be access
 var UserType = graphql.NewObject(graphql.ObjectConfig{
 	Name: "User",
 	Fields: graphql.Fields{
 		"id":        &graphql.Field{Type: graphql.Int},
 		"firstname": &graphql.Field{Type: graphql.String},
 		"lastname":  &graphql.Field{Type: graphql.String},
+		"roles": &graphql.Field{
+			Type: graphql.NewList(roleType),
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				var role []Role
+
+				// userID := params.Source.(User).ID
+				// Implement logic to retrieve user associated roles from user id here.
+
+				return role, nil
+			},
+		},
+	},
+})
+
+var roleType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Role",
+	Fields: graphql.Fields{
+		"id":   &graphql.Field{Type: graphql.Int},
+		"name": &graphql.Field{Type: graphql.String},
 	},
 })
