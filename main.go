f56cbbab43eea79eeaf101aece298b367cbe414b
Amos Pascal Amos <amosamospascal@yahoo.com>
2019-10-19 12:52:47 +0000

    Main, entry function

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
-func main()  {
-	
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+
+	shematConfig := graphql.SchemaConfig{
+		Query: graphql.NewObject(graphql.ObjectConfig{
+			Name:   "Rootquery",
+			Fields: nil,
+		}),
+		Mutation: graphql.NewObject(graphql.ObjectConfig{
+			Name:   "Mutation",
+			Fields: nil,
+		}),
+	}
+
+	shemat, err := graphql.NewSchema(shematConfig)
+	if err != nil {
+		log.Error("Failed to create a new shemat", err)
+	}
+
+	httpHandler := handler.new(&handler.Config{
+		shemat: shemat,
+	})
+
+	http.Handle("/", httpHandler)
+
+	port := os.Getenv("port")
+	if port == "" {
+		port = ":8080"
+	}
+	log.Info("listen on port 8080")
+
+	log.Fatalln(http.ListenAndServe(fmt.Sprintf(port), nil))
 }
