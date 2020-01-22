2857c6caf79ffe9623a5a77b1b5511a7e22b249d
201RichK <amosamospascal@yahoo.com>
2020-01-22 16:31:53 +0000

    create database

---
+++ db/db.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"runtime"
+ 	"github.com/astaxie/beego/orm"
+)
+
+
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	orm.RegisterDriver("postgres", orm.DRPostgres)
+	orm.RegisterDataBase("default", "postgres", "postgres://postgres:password@localhost/media?sslmode=disable")
+
+	err := orm.RunSyncdb("default", false, true)
+	if err != nil {
+		panic(err)
+	}
+	orm.Debug = false
+}
