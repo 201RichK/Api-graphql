0e6579cc667fc932c0312c562b3b95f9cab3714c
201RichK <amosamospascal@yahoo.com>
2020-01-24 18:08:08 +0000

    Create actualite_mot_cle.go

---
+++ queries/actualite_mot_cle.go
@@ -0,0 +1,29 @@
+package queries
+
+import (
+	"github.com/201RichK/graphql/types"
+	"github.com/graphql-go/graphql"
+)
+
+//GeRootFiels return all  the available queries
+func GetRootField() graphql.Fields {
+	return graphql.Fields{
+		"actualite_mo_cle": GetMotCleQuery(),
+	}
+}
+
+// GetUserQuery returns the queries available against user type.
+func GetMotCleQuery() *graphql.Field {
+	return &graphql.Field{
+		Type: graphql.NewList(types.MotCle),
+		Resolve: func(param graphql.ResolveParams) (interface{}, error) {
+			var motCle []*types.ActualiteMotCle
+
+			motCle, err := types.SelectAllMotCle()
+			if err != nil {
+				return nil, err
+			}
+			return &motCle, nil
+		},
+	}
+}
