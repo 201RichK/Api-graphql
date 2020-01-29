c85c7f5bdacc909e518d358959113e34b7c0a8e6
201RichK <amosamospascal@yahoo.com>
2020-01-29 10:50:05 +0000

    Update main.go

--- main.go
+++ main.go
@@ -46,9 +46,9 @@ func main() {
 
 	port := os.Getenv("port")
 	if port == "" {
-		port = ":8080"
+		port = ":8090"
 	}
-	log.Info("listen on port 8080")
+	log.Info("listen on port 8090")
 
 	log.Fatalln(http.ListenAndServe(fmt.Sprintf(port), nil))
 }
