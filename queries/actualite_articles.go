51493713cdf78a57adb1d3596d217603556a49e2
201RichK <amosamospascal@yahoo.com>
2020-01-30 16:41:14 +0000

    actualite article

---
+++ queries/actualite_articles.go
@@ -0,0 +1,21 @@
+package queries
+
+import (
+	"github.com/201RichK/graphql/types"
+	"github.com/graphql-go/graphql"
+	"github.com/sirupsen/logrus"
+)
+
+func GetArticleQuery() *graphql.Field {
+	return &graphql.Field{
+		Type: graphql.NewList(types.Article),
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			articles, err := types.SelectAllArticle()
+			if err != nil {
+				logrus.Info("error select all articles ", err)
+				return nil, nil
+			}
+			return articles, nil
+		},
+	}
+}
