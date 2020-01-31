e02a89951387df6aedda6d77f6b80e3f87437a78
201RichK <amosamospascal@yahoo.com>
2020-01-31 15:01:51 +0000

    Update actualite_mot_cle.go

--- queries/actualite_mot_cle.go
+++ queries/actualite_mot_cle.go
@@ -8,9 +8,24 @@ import (
 // GetUserQuery returns the queries available against user type.
 func GetMotCleQuery() *graphql.Field {
 	return &graphql.Field{
-		Type:        graphql.NewList(types.MotCle),
+		Type: graphql.NewList(types.MotCle),
+		Args: graphql.FieldConfigArgument{
+			"id": &graphql.ArgumentConfig{
+				Type:        graphql.Int,
+				Description: "Include ID to get by id",
+			},
+		},
 		Description: "Get mot cle list",
 		Resolve: func(param graphql.ResolveParams) (interface{}, error) {
+
+			if id, ok := param.Args["id"].(int); ok {
+				ctgr, err := types.SelectMocleId(id, "id")
+				if err != nil {
+					return nil, err
+				}
+				return ctgr, nil
+			}
+
 			motCle, err := types.SelectAllMotCle()
 			if err != nil {
 				return nil, nil
