e79ce43594e630c45f7083dfebae0d71ce543b20
201RichK <amosamospascal@yahoo.com>
2020-01-31 15:01:53 +0000

    Update actualite_articles.go

--- queries/actualite_articles.go
+++ queries/actualite_articles.go
@@ -8,9 +8,23 @@ import (
 
 func GetArticleQuery() *graphql.Field {
 	return &graphql.Field{
-		Type:        graphql.NewList(types.Article),
+		Type: graphql.NewList(types.Article),
+		Args: graphql.FieldConfigArgument{
+			"id": &graphql.ArgumentConfig{
+				Type: graphql.Int,
+			},
+		},
 		Description: "Get articles list",
 		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+
+			if id, ok := p.Args["id"].(int); ok {
+				atcl, err := types.SelectAllArtId(id, "id")
+				if err != nil {
+					return nil, err
+				}
+				return atcl, nil
+			}
+
 			articles, err := types.SelectAllArticle()
 			if err != nil {
 				logrus.Info("error select all articles ", err)
