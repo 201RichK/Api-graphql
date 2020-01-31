6a4a4ae1834a19529cc9e60252a5f019d710555a
201RichK <amosamospascal@yahoo.com>
2020-01-31 15:01:49 +0000

    Update actualiteCategorie.go

--- queries/actualiteCategorie.go
+++ queries/actualiteCategorie.go
@@ -3,20 +3,34 @@ package queries
 import (
 	"github.com/201RichK/graphql/types"
 	"github.com/graphql-go/graphql"
-	"github.com/siddontang/go/log"
+	log "github.com/sirupsen/logrus"
 )
 
 func GetCateoriesQueries() *graphql.Field {
 	return &graphql.Field{
-		Type:        graphql.NewList(types.Catgr),
+		Type: graphql.NewList(types.Catgr),
+		Args: graphql.FieldConfigArgument{
+			"id": &graphql.ArgumentConfig{
+				Type:        graphql.Int,
+				Description: "include Id to get by Id",
+			},
+		},
 		Description: "Get categories list",
-		Resolve: func(p graphql.ResolveParams) (ctgrs interface{}, err error) {
-			ctgrs, err = types.SelectAllCtgr()
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			if id, okay := p.Args["id"].(int); okay {
+				ctgr, err := types.SelectAllctgrId(id, "id")
+				if err != nil {
+					return nil, err
+				}
+				return ctgr, nil
+			}
+
+			ctgrs, err := types.SelectAllCtgr()
 			if err != nil {
 				log.Error("error getting all  categories ", err)
-				return
+				return nil, err
 			}
-			return
+			return ctgrs, nil
 		},
 	}
 }
