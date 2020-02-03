0dd5161328f0fc045897dad68b894150f2646a5d
201RichK <amosamospascal@yahoo.com>
2020-02-03 12:17:28 +0000

    Create actualite_categorie.go

---
+++ mutations/actualite_categorie.go
@@ -0,0 +1,37 @@
+package mutation
+
+import (
+	"github.com/201RichK/graphql/types"
+	"github.com/graphql-go/graphql"
+	"time"
+)
+
+func CreateCagorie() *graphql.Field {
+	return &graphql.Field{
+		Type:        types.Catgr,
+		Description: "Create actualite categories",
+		Args: graphql.FieldConfigArgument{
+			"Nom": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(graphql.String),
+			},
+			"Statut": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(graphql.Boolean),
+			},
+		},
+		Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+			catgr := types.ActualiteCategorie{
+				Nom:       params.Args["Nom"].(string),
+				Statut:    params.Args["statut"].(bool),
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+			}
+
+			err := types.AddActualiteCtgr(&catgr)
+			if err != nil {
+				return nil, err
+			}
+
+			return catgr, nil
+		},
+	}
+}
