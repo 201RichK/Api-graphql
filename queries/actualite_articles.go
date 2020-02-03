94832bb0cef05d33a2e2ebd78955ce3bb2e1b4a6
201RichK <amosamospascal@yahoo.com>
2020-02-03 10:43:17 +0000

    Update actualite_articles.go

--- queries/actualite_articles.go
+++ queries/actualite_articles.go
@@ -11,7 +11,12 @@ func GetArticleQuery() *graphql.Field {
 		Type: graphql.NewList(types.Article),
 		Args: graphql.FieldConfigArgument{
 			"id": &graphql.ArgumentConfig{
-				Type: graphql.Int,
+				Type:        graphql.Int,
+				Description: "Id to get all artticle by id",
+			},
+			"categorieId": &graphql.ArgumentConfig{
+				Type:        graphql.Int,
+				Description: "Categorie Id to get all articles by categorieId",
 			},
 		},
 		Description: "Get articles list",
@@ -23,6 +28,14 @@ func GetArticleQuery() *graphql.Field {
 					return nil, err
 				}
 				return atcl, nil
+			}
+
+			if ctgrID, ok := p.Args["categorieId"].(int); ok {
+				artcl, err := types.SelectAllArtId(ctgrID, "categorie_id")
+				if err != nil {
+					return nil, err
+				}
+				return artcl, nil
 			}
 
 			articles, err := types.SelectAllArticle()
