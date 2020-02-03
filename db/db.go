e3db056ea5beff6367d5ade3e19bb8a83a23e3c7
201RichK <amosamospascal@yahoo.com>
2020-02-03 15:30:34 +0000

    Update db.go

--- db/db.go
+++ db/db.go
@@ -1,8 +1,10 @@
 package db
 
 import (
+	"fmt"
 	"runtime"
 
+	"github.com/201RichK/graphql/utils"
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
@@ -18,7 +20,9 @@ func init() {
 }
 
 func Conn() (*gorm.DB, error) {
-	Db, err = gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=media password=password sslmode=disable")
+	conf := utils.Conf()
+
+	Db, err = gorm.Open("postgres", fmt.Sprintf("host=%s user=%s port=%s password=%s dbname=%s sslmode=%s", conf.Server.Host, conf.Database.Username, conf.Database.DbPort, conf.Database.Password, conf.Database.Dbname, conf.Database.Sslmode))
 	if err != nil {
 		logrus.Error("Gorm Database connection failed to open")
 		return nil, err
